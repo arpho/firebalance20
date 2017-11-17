66ef2dc8764a249703b848647ec1a77f5af540bc
Unknown <damicogiuseppe77@gmail.com>
2017-11-17 20:05:58 +0100

    paymentPage
    reso il pulsante fab fisso e iniziato sviluppo filtro con active sheet

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -2,7 +2,9 @@ import { PaymentsProvider } from '../../providers/payments/payments';
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams,ModalController } from 'ionic-angular';
 import { TabsNavigationPage } from '../tabs-navigation/tabs-navigation';
-import {CreatePaymentPage} from '../create-payment/create-payment'
+import {CreatePaymentPage} from '../create-payment/create-payment';
+import { ActionSheetController } from 'ionic-angular'
+import { ShoppingCartModel } from '../../models/shoppingCart.model';
 
 /**
  * Generated class for the PaymentPage page.
@@ -17,8 +19,10 @@ import {CreatePaymentPage} from '../create-payment/create-payment'
 })
 export class PaymentPage implements OnInit {
   public paymentsList: any;
+  public filterFunction:(doFilter:ShoppingCartModel)=>boolean
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public modal: ModalController,
+    public actionSheetCtrl: ActionSheetController,
     public Payments: PaymentsProvider
   ) {
 
@@ -29,6 +33,56 @@ export class PaymentPage implements OnInit {
       console.log('got payments', payments)
       this.paymentsList = payments;
     })
+  }
+  filter(){
+    console.log('filter');
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'limita a ',
+      enableBackdropDismiss:true,
+      buttons: [
+        {
+          text: 'un giorno',
+          handler: () => {
+            console.log('un giorno clicked');
+            let today = new Date();
+            //tomorrow = new Date(today.getTime()+ (1000*60*60*24))
+            this.filterFunction =(target:ShoppingCartModel)=>{return new Date(target.dataAddebito).getTime()>=today.setDate(today.getDate()-1).getTime()}
+          }
+        },
+        {
+          text: 'una settimana',
+          handler: () => {
+            console.log('un giorno clicked');
+          }
+        },
+        {
+          text: 'un mese',
+          handler: () => {
+            console.log('un mese clicked');
+          }
+        },
+        {
+          text: 'tre mesi',
+          handler: () => {
+            console.log('tre mesi clicked');
+          }
+        },
+        {
+          text: 'un anno',
+          handler: () => {
+            console.log('un anno clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present()
   }
 
 
